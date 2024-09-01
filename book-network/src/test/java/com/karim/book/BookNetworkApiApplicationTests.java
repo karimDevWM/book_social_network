bd088397126c0c8cfdb3dfd05e5351324b1777ca
devjava69 <k.trabelsi9@laposte.net>
2024-09-01 16:31:57 +0100

    update test file

--- book-network/src/test/java/com/karim/book/BookNetworkApiApplicationTests.java
+++ book-network/src/test/java/com/karim/book/BookNetworkApiApplicationTests.java
@@ -3,7 +3,7 @@ package com.karim.book;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BookNetworkApiApplication.class)
 class BookNetworkApiApplicationTests {
 
 	@Test
